a505744867f4ef5b88805e834a5c274bb06e2009
svetad <sv.dmytr@gmail.com>
2020-11-13 14:18:55 +0200

    Fixed link

--- examples/exampleCRUD.cpp
+++ examples/exampleCRUD.cpp
@@ -4,7 +4,7 @@
 #include "jsonarango/arangocollection.h"
 #include "jsonarango/arangoexception.h"
 #include <velocypack/Collection.h>
-#include "jsonarango/arangocurl.h"
+#include "arangocurl.h"
 
 int main(int, char* [])
 {
